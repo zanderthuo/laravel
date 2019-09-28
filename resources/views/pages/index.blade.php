9f464329222474435770fea62f17305c64a585bc
zanderthuo <thuozander@gmail.com>
2019-09-29 01:41:02 +0300

    sandbox

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="jumbotron">
+        <h1>Laravel</h1>
+        <p>This is my first laravel project. I'm excited about it..</p>
+        <p><a class="btn btn-primary btn-lg" href="/sandbox/public/login" role="button">Login</a><a class="btn btn-success btn-lg" href="/sandbox/public/register" role="button">Register</a></p>    
+    </div>
+@endsection
